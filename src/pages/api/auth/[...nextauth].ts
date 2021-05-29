9db750eb0deb973d7585f3321b59057412260a60
Jamil Zazu <jamillzazu@gmail.com>
2021-05-29 10:08:50 -0400

    feat: Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { query as q } from 'faunadb'
 
 import NextAuth from 'next-auth'
+import { session } from 'next-auth/client'
 import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fauna'
@@ -13,18 +14,31 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
         return true
